60218171534733557feb6cc1c91f13982e0299b3
Travis <travmcneil@gmail.com>
2022-02-27 19:21:51 -0500

    first push of my new portfolio website

---
+++ app/static/js/scripts.js
@@ -0,0 +1,18 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLink = document.querySelectorAll('.nav-link')
+
+navToggle.addEventListener('click', () =>{
+    document.body.classList.toggle('nav-open');
+});
+
+
+navLink.forEach(link =>{
+    link.addEventListener('click', () =>{
+        document.body.classList.remove('nav-open');
+    })
+})
---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+#: Base directory of this file
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # i might want to add a postgress conneciton later
